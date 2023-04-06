ce087da456e63512106ec20131990734350c9b7c
Raphael <phael92br@gmail.com>
2023-04-05 22:01:34 -0300

    services  done

--- src/sections/services/Services.jsx
+++ src/sections/services/Services.jsx
@@ -1,8 +1,25 @@
 import './services.css'
+import data from './data'
+import Card from '../../components/Card'
 
 const Services = () => {
   return (
-    <section id="services"><h2>Services</h2></section>
+    <section id="services"><h2>My services</h2>
+    <p>I give you the best in all the services below</p>
+    <div className="container services__container">
+      {
+        data.map(item => (
+          <Card key={item.id} className="service light">
+            <div className="service__icon">{item.icon}</div>
+            <div className="service__details">
+              <h4>{item.title}</h4>
+              <p>{item.desc}</p>
+            </div>
+          </Card>
+        ))
+      }
+    </div>
+    </section>
   )
 }
 
---
+++ src/sections/services/data.js
@@ -0,0 +1,13 @@
+import {SiFigma} from 'react-icons/si'
+import {RiReactjsLine} from 'react-icons/ri'
+import {FaServer} from 'react-icons/fa'
+import {AiFillAppstore} from 'react-icons/ai'
+
+const data = [
+    {id: 1, icon: <SiFigma/>, title: "UI/UX Design", desc: "My designs are modern and intuitive"},
+    {id: 2, icon: <RiReactjsLine/>, title: "Frontend Development", desc: "My designs are modern and intuitive"},
+    {id: 3, icon: <FaServer/>, title: "UI/UX Design", desc: "My designs are modern and intuitive"},
+    {id: 4, icon: <AiFillAppstore/>, title: "UI/UX Design", desc: "My designs are modern and intuitiveMy designs are modern and intuitive"},
+]
+
+export default data
