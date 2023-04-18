756d497bdf6dcc493adfd7e0b83f62be78558413
Raphael <phael92br@gmail.com>
2023-04-18 19:11:13 -0300

    contact section done

--- src/sections/contact/Contact.jsx
+++ src/sections/contact/Contact.jsx
@@ -1,8 +1,23 @@
-import './contact.css'
+import "./contact.css"
+import data from "./data"
+// import Contact from './Contact';
+
 
 const Contact = () => {
   return (
-    <section id="contact"><h2>Contacts</h2></section>
+    <section id="contact">
+      <h2>Get In Touch</h2>
+      <p>
+        Lorem ipsum dolor sit.
+      </p>
+      <div className="container contact__container">
+        {
+          data.map(contact => <a key={contact.id} href={contact.link} target="_blank" rel="noopener noreferrer">
+            {contact.icon}
+          </a>)
+        }
+      </div>
+    </section>
   )
 }
 
---
+++ src/sections/contact/data.js
@@ -0,0 +1,15 @@
+import {HiOutlineMail} from "react-icons/hi"
+import {RiMessengerLine} from "react-icons/ri"
+import {FaWhatsapp} from "react-icons/fa"
+
+const data = [
+    {id: 1, icon: <HiOutlineMail/>, link: "phael92br@gmail.com"},
+    {id: 2, icon: <RiMessengerLine/>, link: "phael92br@gmail.com"},
+    {id: 3, icon: <FaWhatsapp/>, link: "phael92br@gmail.com"},
+    
+]
+
+export default data
+
+// https://wa.me/numberhere
+// https://api.whatsapp.com/send/?phone=%2222222222
